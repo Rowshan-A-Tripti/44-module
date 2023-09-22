ecc73823f65cb15d825612dd16aa415170748d13
tripti <138525545+jhunkar@users.noreply.github.com>
2023-09-22 12:05:40 -0700

    addded features

--- src/Component/PriceOption/PriceOption.jsx
+++ src/Component/PriceOption/PriceOption.jsx
@@ -1,13 +1,21 @@
 import PropTypes from "prop-types";
+import Feature from "../feature/feature";
 
-const PriceOption = () => {
+const PriceOption = ({option}) => {
+    const {name, price, features} = option;
     return (
-        <div>
-            
+        <div className="bg-blue-500 rounded-md p-4 text-black">
+            <h2>
+                <span className="text-7xl font-extrabold">{price}</span>
+                <span className="text-2xl">/mon</span>
+            </h2>
+            <h4 className="text-3xl text-center my-8">{name}</h4>
+            {
+                features.map((feature, index) => <Feature key={index} feature={feature}></Feature>)
+            }
         </div>
     );
 };
-
 
 PriceOption.propTypes = {
     option: PropTypes.object
